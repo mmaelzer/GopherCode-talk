674a69f21f3f35deabf8b0aced60fe8ed1e3ff48
Mike Maelzer <mmaelzer@gmail.com>
2015-10-30 21:36:11 -0700

    Add string/boolean/array/slice/map examples

---
+++ strings.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+// START OMIT
+n := "this is a string 😅 "
+
+fmt.Println(n)
+
+m := `
+Hey look, a
+multiline
+string. Yay.`
+
+fmt.Println(m)
+// END OMIT
+}
